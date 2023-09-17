6ea371e95c44dd2bf03234a2b6044c1805719f15
Mykhailo74 <130218368+Mykhailo74@users.noreply.github.com>
2023-09-17 17:31:46 +0300

    Fix 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,6 @@
-import flatpickr from 'flatpickr';
-import 'flatpickr/dist/flatpickr.min.css';
-import { Report } from 'notiflix/build/notiflix-report-aio';
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import { Report } from "notiflix/build/notiflix-report-aio";
 
 document.body.style.backgroundColor = '#ece5da';
 const TIMER_DELAY = 1000;
