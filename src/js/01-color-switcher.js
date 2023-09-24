e2665f32dfa244ea22852acd398737dcdd3a42a4
Mykhailo74 <130218368+Mykhailo74@users.noreply.github.com>
2023-09-24 21:35:59 +0300

    Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -9,34 +9,23 @@ stopButton.addEventListener('click', stopChangingColor);
 
 function startChangingColor() {
   if (!intervalId) {
-    startButton.disabled = true; // Деактивуємо кнопку "Start"
-    intervalId = setInterval(changeBackgroundColor, 1000); // Змінюємо колір кожну секунду (1000 мс)
+    startButton.disabled = true; 
+    intervalId = setInterval(changeBackgroundColor, 1000); 
   }
 }
 
 function stopChangingColor() {
   if (intervalId) {
-    clearInterval(intervalId); // Зупиняємо інтервал
+    clearInterval(intervalId); 
     intervalId = null;
-    startButton.disabled = false; // Активуємо кнопку "Start" знову
+    startButton.disabled = false; 
   }
 }
 
 function changeBackgroundColor() {
-  const randomColor = getRandomColor();
+  const randomColor = getRandomHexColor();
   body.style.backgroundColor = randomColor;
 }
-
-// function getRandomColor() {
-//   const letters = '0123456789ABCDEF';
-//   let color = '#';
-//   for (let i = 0; i < 6; i++) {
-//     color += letters[Math.floor(Math.random() * 16)];
-//   }
-//   return color;
-// }
-
-
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
