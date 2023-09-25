4cab8bdba6e43268b53d0262d125df282e87997c
Mykhailo74 <130218368+Mykhailo74@users.noreply.github.com>
2023-09-25 20:53:57 +0300

    Task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,52 +1,52 @@
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-// Функція для створення промісу зі затримкою
-function createPromise(position, delay) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const result = { position, delay };
-
-      // Використовуємо початковий код для визначення, чи виконувати або відхиляти проміс
-      if (position % 2 === 0) {
-        resolve(result);
-      } else {
-        reject(result);
+ // Функція для створення промісу зі затримкою
+      function createPromise(position, delay) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            const result = { position, delay };
+            const shouldResolve = Math.random() > 0.3;
+            if (shouldResolve) {
+              resolve(result); // Виконувати проміс
+            } else {
+              reject(result); // Відхиляти проміс
+            }
+          }, delay);
+        });
       }
-    }, delay);
-  });
-}
 
-// Отримуємо посилання на форму
-const form = document.getElementById("promiseForm");
+      // Отримуємо посилання на форму
+      const form = document.getElementById("promiseForm");
 
-// Обробник подачі форми
-form.addEventListener("submit", async (e) => {
-  e.preventDefault(); // Забороняємо стандартну подію форми
+      // Обробник подачі форми
+      form.addEventListener("submit", async (e) => {
+        e.preventDefault(); // Забороняємо стандартну подію форми
 
-  const delayInput = form.querySelector('input[name="delay"]');
-  const stepInput = form.querySelector('input[name="step"]');
-  const amountInput = form.querySelector('input[name="amount"]');
+        const delayInput = form.querySelector('input[name="delay"]');
+        const stepInput = form.querySelector('input[name="step"]');
+        const amountInput = form.querySelector('input[name="amount"]');
 
-  const delay = parseInt(delayInput.value);
-  const step = parseInt(stepInput.value);
-  const amount = parseInt(amountInput.value);
+        const delay = parseInt(delayInput.value);
+        const step = parseInt(stepInput.value);
+        const amount = parseInt(amountInput.value);
 
-  const promises = [];
+        const promises = [];
 
-  // Створюємо promises і додаємо їх до масиву
-  for (let i = 0; i < amount; i++) {
-    promises.push(createPromise(i, delay + i * step));
-  }
+        // Створюємо promises і додаємо їх до масиву
+        for (let i = 0; i < amount; i++) {
+          promises.push(createPromise(i, delay + i * step));
+        }
 
-  // Очікуємо виконання всіх promises
-  try {
-    const results = await Promise.all(promises);
-    console.log("All promises resolved:", results);
-  } catch (error) {
-    console.error("At least one promise was rejected:", error);
-  }
-});
-
+        // Очікуємо виконання всіх promises
+        for (const promise of promises) {
+          try {
+            const result = await promise;
+            console.log(`✅ Fulfilled promise ${result.position} in ${result.delay}ms`);
+          } catch (error) {
+            console.log(`❌ Rejected promise ${error.position} in ${error.delay}ms`);
+          }
+        }
+      });
 
 // function createPromise(position, delay) {
 //   const shouldResolve = Math.random() > 0.3;
