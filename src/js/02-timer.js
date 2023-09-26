085280f316d0009bde078854744be2108f922028
Mykhailo74 <130218368+Mykhailo74@users.noreply.github.com>
2023-09-26 21:42:02 +0300

    All

---
+++ src/js/02-timer.js
@@ -0,0 +1,90 @@
+import flatpickr from 'flatpickr';
+import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix';
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
+
+function updateTimerDisplay(timeLeft) {
+  const daysElement = document.querySelector('[data-days]');
+  const hoursElement = document.querySelector('[data-hours]');
+  const minutesElement = document.querySelector('[data-minutes]');
+  const secondsElement = document.querySelector('[data-seconds]');
+
+  daysElement.textContent = addLeadingZero(timeLeft.days);
+  hoursElement.textContent = addLeadingZero(timeLeft.hours);
+  minutesElement.textContent = addLeadingZero(timeLeft.minutes);
+  secondsElement.textContent = addLeadingZero(timeLeft.seconds);
+}
+
+function startTimer(targetDate) {
+  toggleInputDisabled(true); 
+
+  const timerInterval = setInterval(function () {
+    const currentDate = new Date();
+    const timeLeftMs = targetDate - currentDate;
+
+    if (timeLeftMs <= 0) {
+      clearInterval(timerInterval);
+      toggleInputDisabled(false); 
+      updateTimerDisplay({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+      alert('Time is up!');
+      return;
+    }
+
+    const timeLeft = convertMs(timeLeftMs);
+    updateTimerDisplay(timeLeft);
+  }, 1000);
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+
+const datetimePicker = document.getElementById('datetime-picker');
+const startButton = document.querySelector('[data-start]');
+
+function toggleInputDisabled(disabled) {
+  datetimePicker.disabled = disabled;
+}
+
+const flatpickrOptions = {
+  enableTime: true,
+  dateFormat: 'Y-m-d H:i',
+  onClose: function (selectedDates) {
+    if (selectedDates.length > 0) {
+      const selectedDate = selectedDates[0];
+      const currentDate = new Date();
+      
+      if (selectedDate <= currentDate) {
+        alert('Please choose a date in the future');
+        startButton.disabled = true;
+      } else {
+        startButton.disabled = false;
+        startButton.addEventListener('click', function () {
+          startTimer(selectedDate);
+          startButton.disabled = true;
+        });
+      }
+    }
+  },
+};
+
+const flatpickrInstance = flatpickr(datetimePicker, flatpickrOptions);
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,45 +1,49 @@
 import Notiflix from 'notiflix';
 
-      function createPromise(position, delay) {
-        return new Promise((resolve, reject) => {
-          setTimeout(() => {
-            const result = { position, delay };
-            const shouldResolve = Math.random() > 0.3;
-            if (shouldResolve) {
-              resolve(result);
-            } else {
-              reject(result); 
-            }
-          }, delay);
-        });
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const result = { position, delay };
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve(result);
+      } else {
+        reject(result); 
       }
+    }, delay);
+  });
+}
 
-      const form = document.querySelector('.form');
+const form = document.querySelector('.form');
 
-      form.addEventListener("submit", async (e) => {
-        e.preventDefault(); 
+form.addEventListener("submit", async (e) => {
+  e.preventDefault(); 
 
-        const delayInput = form.querySelector('input[name="delay"]');
-        const stepInput = form.querySelector('input[name="step"]');
-        const amountInput = form.querySelector('input[name="amount"]');
+  const delayInput = form.querySelector('input[name="delay"]');
+  const stepInput = form.querySelector('input[name="step"]');
+  const amountInput = form.querySelector('input[name="amount"]');
 
-        const delay = parseInt(delayInput.value);
-        const step = parseInt(stepInput.value);
-        const amount = parseInt(amountInput.value);
+  const delay = parseInt(delayInput.value);
+  const step = parseInt(stepInput.value);
+  const amount = parseInt(amountInput.value);
 
-        const promises = [];
+  if (step < 0 || delay < 0 || amount <= 0) {
+    Notiflix.Notify.failure('❌ Please enter valid values for step, delay, and amount.');
+    return; 
+  }
 
-        for (let i = 0; i < amount; i++) {
-          promises.push(createPromise(i, delay + i * step));
-        }
+  const promises = [];
 
-              for (const promise of promises) {
-          try {
-            const result = await promise;
-            Notiflix.Notify.success`✅ Fulfilled promise ${result.position} in ${result.delay}ms`;
-          } catch (error) {
-            Notiflix.Notify.failure`❌ Rejected promise ${error.position} in ${error.delay}ms`;
-          }
-        }
-      });
+  for (let i = 0; i < amount; i++) {
+    promises.push(createPromise(i, delay + i * step));
+  }
 
+  for (const promise of promises) {
+    try {
+      const result = await promise;
+      Notiflix.Notify.success(`✅ Fulfilled promise ${result.position} in ${result.delay}ms`);
+    } catch (error) {
+      Notiflix.Notify.failure(`❌ Rejected promise ${error.position} in ${error.delay}ms`);
+    }
+  }
+});
